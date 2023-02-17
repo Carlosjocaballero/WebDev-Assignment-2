e24a77fe1b787e8d59e2e88fe274d16ea8f054d7
Carlosjocaballero <carlosjocaballero@gmail.com>
2023-02-16 21:03:03 -0500

    Finished myEvery method

--- javascript-methods.js
+++ javascript-methods.js
@@ -38,7 +38,14 @@ Array.prototype.mySome = function(callbackFn) {
 
 // EVERY //
 Array.prototype.myEvery = function(callbackFn) {
-  // Place your code here.
+	for(let i = 0; i < this.length; i++){
+		if(this[i] === undefined) continue;
+		// callbackFn return boolean
+		if(!callbackFn(this[i], i, this)){
+			return false;	
+		}
+	}
+	return true;
 };
 
 // REDUCE //
